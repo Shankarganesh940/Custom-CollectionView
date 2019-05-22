20d79d377ecb863e5f02242d01133bc064ef8704
Shankar Ganesh <shankarganesh940@gmail.com>
2019-05-22 12:40:42 +0530

    Initial Commit

---
+++ CustomCollectionView/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  CustomCollectionView
+//
+//  Created by Greeens5 on 22/05/19.
+//  Copyright © 2019 Book. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var image1: UIImageView!
+    @IBOutlet var image2: UIImageView!
+    @IBOutlet var label: UILabel!
+    
+}
--- CustomCollectionView/ViewController.swift
+++ CustomCollectionView/ViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
+    
+    var array = ["Dhoni"]
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.image.image = UIImage(named: "Dhoni")
+         cell.image1.image = UIImage(named: "Dhoni1")
+         cell.image2.image = UIImage(named: "Dhoni2")
+        cell.label.text = "Dhoni"
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
